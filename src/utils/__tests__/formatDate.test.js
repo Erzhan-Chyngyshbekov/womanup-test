8dcf1da360984f907bba83b701fead1405c58e3a
erzhan-chyngyshbekov <e4yngywbekov@mail.ru>
2022-11-23 13:30:01 +0600

    deleted

--- src/utils/__tests__/formatDate.test.js
+++
@@ -1,7 +0,0 @@
-import formatDate from '../formatDate';
-
-describe('formatDate', () => {
-  it('formats correctly', () => {
-    expect(formatDate('2020-12-31', 'MM-DD-YYYY')).toBe('12-31-2020');
-  });
-});
